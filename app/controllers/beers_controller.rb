7f12fd8e3b328c50ae8b85c35ed66607482095f9
borourke <borourke@crowdflower.com>
2016-06-04 17:36:51 -0700

    add description poc

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,15 +1,22 @@
 class BeersController < ApplicationController
   def main
     beer = params[:request][:intent][:slots][:Beer][:value]
+    description = get_description(beer_name: beer)
     render json: { 
       version: "1.0",
       response: {
         outputSpeech: {
           type: "PlainText",
-          text: "#{beer} sure is tasty."
+          text: description
         },
         shouldEndSession: true
       }
     }
+  end
+
+  def get_description(beer_name:)
+    response = `curl -X GET 'http://api.brewerydb.com/v2/search?q=budweiser&type=beer&key=7c232372effd9a6ae4edf1494e8e1901'`
+    parsed = JSON.parse(response)
+    parsed["data"].first["description"]
   end
 end
